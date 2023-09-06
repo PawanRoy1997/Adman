e402b702626b7c47fc5b3c14ba446170d9eff253
Pawan <pawanroy0066@gmail.com>
2023-09-06 15:22:50 +0530

    minor config fix

--- adman/build.gradle.kts
+++ adman/build.gradle.kts
@@ -26,11 +26,11 @@ android {
     }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
 }
 
@@ -38,7 +38,7 @@ afterEvaluate{
     publishing{
         publications {
             create<MavenPublication>("maven"){
-                groupId = "org.github.PawanRoy1997"
+                groupId = "com.github.PawanRoy1997"
                 artifactId = "adman"
                 version = "1.0.0"
             }
